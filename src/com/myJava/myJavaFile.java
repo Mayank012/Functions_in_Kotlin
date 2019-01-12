a713df72ea17072315d0b313b81273e988e7f318
Mayank012 <mayank.chauhan012@gmail.com>
2019-01-12 13:57:39 +0530

    Functions in Kotlin!

---
+++ src/com/myJava/myJavaFile.java
@@ -0,0 +1,21 @@
+//package com.myJava;
+//
+//import com.myKotlin.myCustomKotlinFile;
+//
+//public class myJavaFile {
+//
+//    public static void main(String[] args) {
+//
+////        int sum = myCustomKotlinFile.max(3,4);
+////        System.out.println("Priting com.myKotlin.max from java file: " + sum);
+//
+////        int result = myCustomKotlinFile.findVolume(2,3,40);
+////        System.out.println(result);
+//
+//    }
+//
+////    public static int getArea(int l, int b) {
+////        return l * b;
+////    }
+//
+//}
---
+++ src/com/myKotlin/Kotlin Functions.kt
@@ -0,0 +1,75 @@
+import java.math.BigInteger
+
+//@file:JvmName("myCustomKotlinFile")
+
+//package com.myKotlin
+
+//import com.myJava.myJavaFile
+
+//fun main(args: Array<String>) {
+
+//    var largeValue = max(4, 6)
+//    println("The greater no is ${largeValue}")
+//
+//    var area = myJavaFile.getArea(10,5);
+//    println("Printing area from the kotlin file: " +area)
+
+//    findVolume(height = 30,length = 2,breadth = 3)    // Naming functions
+
+//    var str1: String = "Hello "
+//    var str2: String = "World"
+//
+//    var str3: String = "Hey "
+//
+//    println(str3.add(str1, str2))
+
+//    val x = 6
+//    val y = 10
+//
+//    val greaterVal = x greaterValue y
+//
+//    println(greaterVal)
+
+
+//   println(getFibonacciNumber(10000, BigInteger("1"), BigInteger("0")))
+//}
+
+//fun String.add(s1: String, s2: String): String      // Extension function
+//{
+//
+//    return this + s1 + s2
+//}
+
+//infix fun Int.greaterValue(other: Int): Int      // Infix function & Extension function
+//{
+//    if(this > other)
+//        return this
+//    else
+//        return other
+//}
+
+//fun max(a: Int, b: Int): Int
+//        = if (a > b) {
+//           println("$a is greater")
+//           a
+//        } else {
+//           println("$b is greater")
+//           b
+//        }
+//
+
+//tailrec fun getFibonacciNumber(n: Int, a: BigInteger, b: BigInteger): BigInteger // Tailrec Function
+//{
+//
+//    if (n == 0)
+//        return b
+//    else
+//        return getFibonacciNumber(n - 1, a + b, a)
+//}
+
+//@JvmOverloads
+//fun findVolume(length: Int, breadth: Int, height: Int = 10) {
+//    println("length is $length")
+//    println("breadth is $breadth")
+//    println("height is $height")
+//}
